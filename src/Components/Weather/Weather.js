f21247c6112df54e7100e93f607b2d19dbaf0bb5
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-11-17 20:59:09 +0100

    update file

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -1,33 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './weather.css';
+import axios from 'axios';
 
 const Weather = () => {
+	const [data, setData] = useState({});
+	const [location, setLocation] = useState('');
+	const apiUrl =
+		'https://api.openweathermap.org/data/2.5/weather?q=lagos&units=imperial&appid=fd13aecf47aacf819b0214781879676c';
+
+	const searchLocation = (e) => {
+		if (e.key === 'Enter') {
+			axios.get(apiUrl).then((resp) => {
+				setData(resp.data);
+				console.log(resp.data);
+			});
+			setLocation('');
+		}
+	};
 	return (
 		<>
-			<main>
+			<main className='weather__app'>
 				<div className='weather__input'>
-					<input type='text' placeholder='Enter City' />
+					<input
+						type='text'
+						placeholder='Enter City'
+						value={location}
+						onChange={(e) => setLocation(e.target.value)}
+						onKeyPress={searchLocation}
+					/>
 				</div>
 				<section className='container'>
 					<div className='container__top'>
-						<p>LAGOS</p>
-						<h2 className='weather__temp'>19</h2>
-						<p className='desc'>clear skies</p>
-					</div>
-					<div className='weather__info'>
+						<p className='location'>{data.name}</p>
 						<div>
-							<p>30</p>
-							<p>Humidity</p>
+							{data.main ? (
+								<h2 className='weather__temp'>{data.main.temp.toFixed()}°F</h2>
+							) : null}
 						</div>
 						<div>
-							<p>30</p>
-							<p>Visibility</p>
-						</div>
-						<div>
-							<p>30</p>
-							<p>Wind</p>
+							{data.weather ? <p className='desc'>{data.weather[0].main}</p> : null}
 						</div>
 					</div>
+
+					{data.name !== undefined && (
+						<div className='weather__info'>
+							<div>
+								{data.main ? (
+									<p className='weather__emp'>{data.main.humidity}%</p>
+								) : null}
+								<p>Humidity</p>
+							</div>
+							<div>
+								<p className='weather__emp'>{data.visibility.toFixed()}km</p>
+								<p>Visibility</p>
+							</div>
+							<div>
+								{data.wind ? (
+									<p className='weather__emp'>{data.wind.speed.toFixed()}MPH</p>
+								) : null}
+								<p>Wind(Speed)</p>
+							</div>
+						</div>
+					)}
 				</section>
 			</main>
 		</>
